6fb70c7e2eff8f458d54cfa255138bc3f3f99e7d
Danny Brandt <dvbrandt90@gmail.com>
2020-12-03 18:05:59 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,20 @@
 def starts_with_a_vowel?(word)
-
+ if word.scan(/\A[aeiouAEIOU]/)
+   true 
+ else false
+ end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/\w^un/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text.scan(/\A[A-Z]/)
 end
 
 def valid_phone_number?(phone)
