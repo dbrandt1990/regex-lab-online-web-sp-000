e63e3945b4314e05c60ec47b771a849a09a87542
Danny Brandt <dvbrandt90@gmail.com>
2020-12-03 18:18:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,7 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match(/\A[A-Z]/)
+text.match(/\A[A-Z]+\w+[?.!]{1}/)
 end
 
 def valid_phone_number?(phone)
